59adff2b06bbe2322b7cbc5625e8678f6e71a50b
Starkus1999 <thalisson.bandeira@hotmail.com>
2020-12-14 11:45:57 -0500

    Added check and raises for errors & added get_edges_to in node.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 """
+Python 3.7
+
 https://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
 
 https://www.educative.io/edpresso/how-to-implement-depth-first-search-in-python
@@ -15,17 +17,18 @@ n4 = Node("D")
 n5 = Node("E")
 n6 = Node("F")
 
-graph = Graph("A")
+graph = Graph(n1)
 
-n1.vertices = [n2, n3]
-n2.vertices = [n4, n5]
-n3.vertices = [n6]
-n5.vertices = [n6]
+n1.edges_to = [[n2, 5], [n3]]
+n2.edges_to = [[n4, 10], [n5, 25]]
+n3.edges_to = [[n6, 10]]
+n5.edges_to = [[n6, 0]]
 
-print(f"{n1}: {n1.vertices}")
-print(f"{n2}: {n2.vertices}")
-print(f"{n3}: {n3.vertices}")
-print(f"{n4}: {n4.vertices}")
-print(f"{n5}: {n5.vertices}")
-print(f"{n6}: {n6.vertices}")
+print(f"{n1}: {n1.get_edges_to(value=False, with_coust=True)}")
 
+print(f"{n1}: {n1.edges_to}")
+print(f"{n2}: {n2.edges_to}")
+print(f"{n3}: {n3.edges_to}")
+print(f"{n4}: {n4.edges_to}")
+print(f"{n5}: {n5.edges_to}")
+print(f"{n6}: {n6.edges_to}")
--- utils/node.py
+++ utils/node.py
@@ -1,10 +1,12 @@
 
 class Node:
 
-    def __init__(self, value, coust=0, vertices=[]):
+    def __init__(self, value):
+        """
+        :param value: Id or name for the node. Must be unique.
+        """
         self.__value = value
-        self.__coust = coust
-        self.__vertices = vertices
+        self.__edges_to = []
 
     @property
     def value(self):
@@ -15,12 +17,40 @@ class Node:
         return self.__coust
 
     @property
-    def vertices(self):
-        return [node.value for node in self.__vertices]
+    def edges_to(self):
+        return self.__edges_to
 
-    @vertices.setter
-    def vertices(self, list_nodes):
-        self.__vertices = list_nodes
+    def get_edges_to(self, value=True, with_coust=True):
+        """
+        :param value: Return list with nodes values instead of Node Object.
+        :param with_coust: Return list with coust to a Node Object.
+        :return: a list of list that each list represents an edge with node and coust.
+        """
+        if value:
+            if with_coust:
+                result = []
+                for node in self.__edges_to:
+                    result.append([node[0].value, node[1]])
+                return result
+        return self.__edges_to if with_coust and value is False else [node[0].value for node in self.__edges_to]
+
+    @edges_to.setter
+    def edges_to(self, list_nodes):
+        """
+        :param list_nodes: a list of list that each list represents an edge with node and coust. If coust not informed,
+        the default value is 0.
+        :return: edge_to variable value.
+        """
+        for node in list_nodes:
+            if len(node) == 1:
+                self.__edges_to.append([node[0], 0])
+            else:
+                if not isinstance(node[0], Node):
+                    raise Exception("First value must be a node object")
+                if not isinstance(node[1], int):
+                    raise Exception("Second value must be an int number")
+                self.__edges_to.append(node)
+        return self.__edges_to
 
     def __str__(self):
         return self.__value
